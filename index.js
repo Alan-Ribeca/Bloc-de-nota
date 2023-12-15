e97cdac942f19115d39fa73eddf952526502d965
Alan Ribeca <alanribeca2003@gmail.com>
2023-12-15 19:19:21 -0300

    dandole retoques a las card

--- index.js
+++ index.js
@@ -3,38 +3,73 @@ const botonLapiz = document.querySelector(".editBtn");
 const bloc = document.querySelector(".notasBloc");
 const flechaGuardar = document.querySelector(".flechaBloc");
 const divCard = document.querySelector(".card")
+let tarjetas = JSON.parse(localStorage.getItem("tarjetas")) || [];
+let cardId = tarjetas.length;
+
 
 // addEventListener
+
+window.onload = function () {
+    generarTarjetas();
+}
+
 botonLapiz.addEventListener("click", () => {
     clickLapiz();
 })
 
+flechaGuardar.addEventListener("click", () => {
+    clickFlecha();
+})
+
 // funciones
+function generarTarjetas() {
+    const divHTML = document.querySelector(".cardEchas");
+    divHTML.innerHTML = ""; 
+    for (let tarjeta of tarjetas) {
+        const contenedorBloc = document.createElement("div");
+        contenedorBloc.classList.add("cardConTexto");
+        contenedorBloc.id = "card" + tarjeta.id;
+
+        const h1Input = document.createElement("H1");
+        h1Input.classList.add("h1Input");
+        h1Input.textContent = tarjeta.h1;
+        contenedorBloc.appendChild(h1Input);
+
+        const pInput = document.createElement("P");
+        pInput.classList.add("pInput");
+        pInput.textContent = tarjeta.p;
+        contenedorBloc.appendChild(pInput);
+
+        // agregar bnt de eliminar en card
+        const btnDelete = document.createElement("button");
+        btnDelete.innerHTML = "Eliminar";
+        contenedorBloc.appendChild(btnDelete);
+
+        divHTML.appendChild(contenedorBloc);
+    }
+}
 
 bloc.style.display = "none";
 botonLapiz.style.display = "block"
 divCard.style.display = "block"
-function clickLapiz(){
-    if(bloc.style.display === "none") {
+
+function clickLapiz() {
+    if (bloc.style.display === "none") {
         bloc.style.display = "block"
     } else {
         bloc.style.display = "none"
     }
 
-    if(botonLapiz.style.display === "block") {
+    if (botonLapiz.style.display === "block") {
         botonLapiz.style.display = "none"
     } else {
         botonLapiz.style.display = "block"
     }
 
-    if(divCard.style.display === "block") {
+    if (divCard.style.display === "block") {
         divCard.style.display = "none"
-    } 
+    }
 }
-
-flechaGuardar.addEventListener("click", () => {
-    clickFlecha();
-})
 
 function clickFlecha() {
     // ocultar otra vez el bloc
@@ -43,39 +78,26 @@ function clickFlecha() {
 
     // guardar los datos del input en localStorage 
     const inputBloc = document.querySelector(".textarea");
-    localStorage.setItem("inputBloc", inputBloc.value);
     const h1Card = document.querySelector(".titulo-Bloc");
-    localStorage.setItem("h1Card", h1Card.value);
+
+    // Agrega los datos de la nueva tarjeta al array
+    tarjetas.push({
+        id: ++cardId,
+        h1: h1Card.value,
+        p: inputBloc.value
+    });
+
+    // Guarda el array actualizado en localStorage
+    localStorage.setItem("tarjetas", JSON.stringify(tarjetas));
 
     // resetear los input
     inputBloc.value = "";
     h1Card.value = "";
-    
-    // creo el contenedor de las card
-    const contenedorBloc = document.createElement("div");
-    contenedorBloc.classList.add("cardConTexto")
-    
-    // obtengo los datos del localStorage
-    let datos = localStorage.getItem("inputBloc");
-    let datosH1 = localStorage.getItem("h1Card");
-    
-    // creo el h1, lo completo con los datos y lo agrego
-    const h1Input = document.createElement("H1");
-    h1Input.classList.add("h1Input");
-    h1Input.textContent = datosH1;
-    contenedorBloc.appendChild(h1Input)
 
-    // creo un elemento P para el texto, lo completo con los datos y lo agrego
-    const pInput = document.createElement("P");
-    pInput.classList.add("pInput");
-    pInput.textContent = datos;
-    contenedorBloc.appendChild(pInput);
+    // Recrea todas las tarjetas
+    generarTarjetas();
 
-    // agrego los dos input al html
-    const divHTML = document.querySelector(".cardEchas");
-    divHTML.appendChild(contenedorBloc);
-
-    if(divCard.style.display === "none") {
+    if (divCard.style.display === "none") {
         divCard.style.display = "block"
     }
 }
