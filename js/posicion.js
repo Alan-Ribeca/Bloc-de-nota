e6aef1c4bcc5f95742d9ed7d1daa6fa9674c895d
Alan Ribeca <alanribeca2003@gmail.com>
2024-01-14 16:11:13 -0300

    agregandole la funcion de grid a las cards

--- js/posicion.js
+++ js/posicion.js
@@ -1,8 +1,10 @@
 export {}
 
 const linea = document.querySelector(".container");
+const card = document.querySelector(".cardEchas");
 
 linea.addEventListener("click", (e) => {
     e.preventDefault();
     console.log("click");
+    card.classList.toggle("grid")
 });
